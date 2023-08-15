abfa507375c6eb7db7f28ff250ac565b25fe4254
Nick Santos <nickrsan@users.noreply.github.com>
2023-08-15 12:11:37 -0700

    Fixing numerous a few items from code review
    Moved the original test to an example, created a basic new test that should help ID basic failures importing the package, at least

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,11 @@
 # import ee
 # from ee import ImageCollection
-# import pytest
-# import eedl
+import pytest
+from eedl.image import Image
 
 # we should change the name of our Image class - it conflicts with the class image in the ee package, and people will
 # likely be using both. Let's not cause confusion
+
+def test_class_instantiation():
+	image = Image()
+	assert type(image) == Image
