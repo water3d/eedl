6a14f84af048be61e44ee13ecb153a5b459b7115
Nick Santos <nickrsan@users.noreply.github.com>
2023-08-16 15:53:46 -0700

    minor: rename polygons to features

--- tests/test_zonal_centroids.py
+++ tests/test_zonal_centroids.py
@@ -5,13 +5,13 @@ from eedl import zonal
 
 
 def test_zonal_centroids():
-	polygons = r"C:\Users\dsx\CodeLocal\eedl\tests\data\test_vectors.gpkg\test_polys_centroids"
+	features = r"C:\Users\dsx\CodeLocal\eedl\tests\data\test_vectors.gpkg\test_polys_centroids"
 	raster = r"C:\Users\dsx\CodeLocal\eedl\tests\data\_ee_export_test_image.tif"
 	output_folder = r"C:\Users\dsx\CodeLocal\eedl\tests\test_outputs"
 	filename = "test_results"
 	keep_fields = ("UniqueID", "expected_centroid_value")
 	stats = ()
-	output_csv = zonal.zonal_stats(polygons, raster, output_folder, filename, keep_fields, stats, use_points=True)
+	output_csv = zonal.zonal_stats(features, raster, output_folder, filename, keep_fields, stats, use_points=True)
 
 	results = pandas.read_csv(output_csv)
 	pandas.testing.assert_series_equal(
