828c14bc9aa1c81fb1a8ff68c105a2d14988aeab
Nick Santos <nickrsan@users.noreply.github.com>
2023-09-20 10:44:07 -0700

    GDAL works, move conf file, update docs requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,7 @@
 
 import sys
 import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
