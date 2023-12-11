d37526871dcd2ff9070d872631cac9ca8ac8f22d
Adam Crawford <adamcrawford253@gmail.com>
2023-12-10 23:16:34 -0800

    Pass lint check.

--- ci/version_checker.py
+++ ci/version_checker.py
@@ -16,9 +16,9 @@ assert version_number == setupcfg_version
 assert version_number > latest
 
 """
-This Regular Expression does a few things. 
+This Regular Expression does a few things.
 It makes sure the format is x.yyyy.mm.dd where x is any number of consecutive digits.
-It makes sure there are no leading 0s except for possibly in the 'x' part of the version number. ie 0.2023.12.9 is valid but 0.2023.12.09 is not valid. 
+It makes sure there are no leading 0s except for possibly in the 'x' part of the version number. ie 0.2023.12.9 is valid but 0.2023.12.09 is not valid.
 It also checks that the month and day are valid -- month is in [1, 2, 3, ..., 12] and day is in [1, 2, 3, ..., 31].
 """
 version_pattern = re.compile(r"^\d*\.\d{4}\.(1[0-2]|[1-9])\.(3[01]|[12][0-9]|[1-9])$")
